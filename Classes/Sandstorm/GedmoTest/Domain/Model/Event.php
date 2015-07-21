d14cc022cb9846ff1836290d8b913ca2b79d2b05
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm-media.de>
2015-07-21 16:02:35 +0200

    [TASK] example for translating assets

--- Classes/Sandstorm/GedmoTest/Domain/Model/Event.php
+++ Classes/Sandstorm/GedmoTest/Domain/Model/Event.php
@@ -11,6 +11,10 @@ use TYPO3\Flow\Annotations as Flow;
 use Doctrine\ORM\Mapping as ORM;
 
 use Gedmo\Mapping\Annotation as Gedmo;
+use TYPO3\Flow\Persistence\Generic\PersistenceManager;
+use TYPO3\Flow\Persistence\PersistenceManagerInterface;
+use TYPO3\Flow\Property\PropertyMapper;
+use TYPO3\Media\Domain\Repository\AssetRepository;
 
 /**
  * @Flow\Entity
@@ -19,10 +23,42 @@ class Event {
 	use TranslatableTrait;
 
 	/**
+	 * @var array
+	 * @Flow\Transient
+	 */
+	protected $translationAssociationMapping = array(
+		'assetIdentifier' => 'asset'
+	);
+
+	/**
 	 * @Gedmo\Translatable
 	 * @var string
 	 */
 	protected $name;
+
+	/**
+	 * @Gedmo\Translatable
+	 * @var string
+	 */
+	protected $assetIdentifier;
+
+	/**
+	 * @Flow\Inject
+	 * @var AssetRepository
+	 */
+	protected $assetRepository;
+
+	/**
+	 * @Flow\Inject
+	 * @var PersistenceManagerInterface
+	 */
+	protected $persistenceManager;
+
+	/**
+	 * @Flow\Inject
+	 * @var PropertyMapper
+	 */
+	protected $propertyMapper;
 
 	/**
 	 * @return string
@@ -37,5 +73,40 @@ class Event {
 	 */
 	public function setName($name) {
 		$this->name = $name;
+	}
+
+	/**
+	 * @return \TYPO3\Media\Domain\Model\Asset
+	 */
+	public function getAsset() {
+		return $this->assetOnLoad($this->assetIdentifier);
+	}
+
+	/**
+	 * @param array $asset
+	 */
+	public function setAsset($asset) {
+		$this->assetIdentifier = $this->assetOnSave($asset);
+	}
+
+	/**
+	 * @param array $asset
+	 */
+	public function assetOnSave($asset) {
+		$asset = $this->propertyMapper->convert($asset, 'TYPO3\Media\Domain\Model\AssetInterface');
+		if ($asset === NULL) {
+			$this->assetRepository->remove($asset);
+			return NULL;
+		} elseif ($this->persistenceManager->isNewObject($asset)) {
+			$this->assetRepository->add($asset);
+			return $this->persistenceManager->getIdentifierByObject($asset);
+		} else {
+			$this->assetRepository->update($asset);
+			return $this->persistenceManager->getIdentifierByObject($asset);
+		}
+	}
+
+	public function assetOnLoad($assetIdentifier) {
+		return $this->assetRepository->findByIdentifier($assetIdentifier);
 	}
 }
